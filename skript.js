8d43d5c42787bbee98e19cd6da865ea76fba366e
mercer193 <32912460+mercer193@users.noreply.github.com>
2018-08-12 18:08:10 -0400

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,12 +1,12 @@
 window.onscroll = function() {myFunction()};
 
-var sticky-title = document.getElementById("sticky-title");
-var sticky = sticky-title.offsetTop;
+var bar = document.getElementById("bar");
+var sticky = bar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    sticky-title.classList.add("sticky")
+    bar.classList.add("sticky")
   } else {
-    sticky-title.classList.remove("sticky");
+    bar.classList.remove("sticky");
   }
 }
